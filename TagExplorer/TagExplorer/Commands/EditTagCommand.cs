38cc82d0c05e382fb3d28ba61da21f56727065f5
Daniel Morsch <d.morsch.bs@gmail.com>
2018-09-01 15:31:02 +0200

    add command and helpers

---
+++ TagExplorer/TagExplorer/Commands/EditTagCommand.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+using EnvDTE;
+using EnvDTE80;
+using System.Windows.Forms;
+
+namespace TagExplorer.Commands
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class EditTagCommand
+    {
+        public const int CommandId = 256;
+
+        public static readonly Guid CommandSet = new Guid("b616aac5-6b80-4333-8cf3-b531d6974dfa");
+
+        private readonly Package _package;
+
+        private EditTagCommand( Package package )
+        {
+            _package = package;
+
+            var commandService = (OleMenuCommandService) ServiceProvider.GetService( typeof( IMenuCommandService ) );
+
+            if ( commandService != null )
+            {
+                var menuCommandID = new CommandID( CommandSet, CommandId );
+                var menuItem = new MenuCommand( EditTag, menuCommandID );
+                commandService.AddCommand( menuItem );
+            }
+
+        }
+
+        public static EditTagCommand Instance { get; private set; }
+
+        private IServiceProvider ServiceProvider
+        {
+            get { return _package; }
+        }
+
+        public static void Initialize( Package package )
+        {
+            Instance = new EditTagCommand( package );
+        }
+
+
+        private void EditTag( object sender, EventArgs e )
+        {
+            try
+            {
+                var dte = (DTE2) ServiceProvider.GetService( typeof( DTE ) );
+                string path = ProjectHelpers.GetSelectedPath( dte, true );
+
+                if ( !string.IsNullOrEmpty( path ) )
+                {
+                    MessageBox.Show( path );
+                }
+                else
+                {
+                    MessageBox.Show( "Couldn't resolve the folder" );
+                }
+            }
+            catch ( Exception ex )
+            {
+                Logger.Log( ex );
+            }
+        }
+        
+    }
+}
